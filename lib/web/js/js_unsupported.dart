9b09d12c1397966f56ab6bea95cdb862345bbfe6
Jeroen1602 <1492076+jeroen1602@users.noreply.github.com>
2022-01-01 22:42:50 +0100

    Added documentation for all teh public classes, methods and fields for the bluetooth_web_js_utils library and js stub

--- lib/web/js/js_unsupported.dart
+++ lib/web/js/js_unsupported.dart
@@ -1,14 +1,23 @@
-/// @nodoc This is the unsupported version.
+///
+/// A stub implementation of @JS from dart js to make sure that the code
+/// will at least compile for native builds.
+///
 class JS {
   final String? name;
 
   const JS([this.name]);
 }
 
-/// @nodoc
+///
+/// A stub implementation of @anonymouse from dart js to make sure that the code
+/// will at least compile for native builds.
+///
 class _Anonymous {
   const _Anonymous();
 }
 
-/// @nodoc
+///
+/// A stub implementation of @anonymouse from dart js to make sure that the code
+/// will at least compile for native builds.
+///
 const _Anonymous anonymous = _Anonymous();
--- lib/web/js/js_utils_unsupported.dart
+++ lib/web/js/js_utils_unsupported.dart
@@ -1,50 +1,73 @@
-/// @nodoc
+///
+/// A library around dart:js and dart:js_util to help make it testable and
+/// helps making the library compile against dart IO. (It will however not
+/// work).
+///
 library bluetooth_web_js_utils;
+
+import 'package:meta/meta.dart';
 
 part 'js_utils_interface.dart';
 
-/// @nodoc
+///
+/// A stub implementation of [JSUtilsInterface] that will be used in dart native
+/// builds of the project.
+///
+/// Everything results in an exception at runtime, but this allows for it to
+/// be compiled.
+///
+/// The actually used implementation can be overwritten by calling [testingSetJSUtils].
+///
 class JSUtils extends JSUtilsInterface {
+  @alwaysThrows
   @override
-  callConstructor(Object constr, List<Object?>? arguments) {
+  dynamic callConstructor(Object constr, List<Object?>? arguments) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
-  callMethod(Object o, String method, List<Object?> args) {
+  dynamic callMethod(Object o, String method, List<Object?> args) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
-  getProperty(Object o, Object name) {
+  dynamic getProperty(Object o, Object name) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
   bool hasProperty(Object o, Object name) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
   bool instanceof(Object? o, Object type) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
-  newObject() {
+  dynamic newObject() {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
   Future<T> promiseToFuture<T>(Object jsPromise) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
-  setProperty(Object o, Object name, Object? value) {
+  dynamic setProperty(Object o, Object name, Object? value) {
     throw UnimplementedError();
   }
 
+  @alwaysThrows
   @override
   F allowInterop<F extends Function>(F f) {
     throw UnimplementedError();
